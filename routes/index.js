130c5411d0b76f091b9dd2311b1b2b8dd3c3402e
pierre-paul <ptacher@gmail.com>
2020-07-10 14:26:22 +0200

    2 touch gestures.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Fastslam' });
-  res.render('index0');
+  res.render('index0',{cache:false,debug:true});
 });
 
 
